96a400d2dea9386b81d600850ff598462914fe1a
karthik-utk <karthikkarri13@gmail.com>
2026-03-29 21:52:58 +0530

    Add fake weather objects - console log all values

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
+const fakeWeatherObj = [{
+    cityName: 'Vijayawada',
+    Temparture: '38 C',
+    Humidity: '0.75',
+    weatherCondition: 'Sunny',
+    windSpeed: '10 km/h'
+},
+{
+    cityName: 'Hyderabad',
+    Temparture: '19 C',
+    Humidity: '0.15',
+    weatherCondition: 'Cloudy',
+    windSpeed: '3 km/h'
+}, {
+    cityName: 'Chennai',
+    Temparture: '25 C',
+    Humidity: '0.55',
+    weatherCondition: 'Sunny',
+    windSpeed: '13 km/h'
+}]
+
 function getWeather() {
+    let found = 0;
+
     let city = document.getElementById("cityInput").value;
-    console.log(city);
+    fakeWeatherObj.forEach(we => {
+        if (city === we.cityName) {
+            console.log(we.cityName);
+            console.log(we.Temparture);
+            console.log(we.Humidity);
+            console.log(we.weatherCondition);
+            console.log(we.windSpeed);
+            found = 1;
+        }
+    })
+
+    if (found === 0) {
+        console.log('City not found')
+    }
 }
