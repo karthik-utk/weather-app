92630a6fe65474a6b17a8b6c6a10158e868ab41e
karthik-utk <karthikkarri13@gmail.com>
2026-03-31 21:19:47 +0530

    Replace fake data with real API call

--- script.js
+++ script.js
@@ -1,42 +1,24 @@
-const fakeWeatherObj = [{
-    cityName: 'Vijayawada',
-    Temparture: '38 C',
-    Humidity: '0.75',
-    weatherCondition: 'Sunny',
-    windSpeed: '10 km/h'
-},
-{
-    cityName: 'Hyderabad',
-    Temparture: '19 C',
-    Humidity: '0.15',
-    weatherCondition: 'Cloudy',
-    windSpeed: '3 km/h'
-}, {
-    cityName: 'Chennai',
-    Temparture: '25 C',
-    Humidity: '0.55',
-    weatherCondition: 'Sunny',
-    windSpeed: '13 km/h'
-}]
-
 function getWeather() {
-    let result = document.getElementById("weatherResult");
-    let found = false;
     let city = document.getElementById("cityInput").value;
-    fakeWeatherObj.forEach(we => {
-        if (city === we.cityName) {
-            found = true;
-            result.innerHTML = `
-                <p>City: ${we.cityName}</p>
-                <p>Temperature: ${we.Temparture}</p>
-                <p>Humidity: ${we.Humidity}</p>
-                <p>Condition: ${we.weatherCondition}</p>
-                <p>Wind Speed: ${we.windSpeed}</p>
-            `;
-        }
-    })
-
-    if (!found) {
-       result.innerHTML = '<p>City not found</p>';
-    }
+    let result = document.getElementById("weatherResult");
+    let apiKey = "b5f779cab944b9cc9d68f9f3c17905c4";
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.cod === 200) {
+                result.innerHTML = `
+                    <p>City: ${data.name}</p>
+                    <p>Temperature: ${data.main.temp} °C</p>
+                    <p>Humidity: ${data.main.humidity} %</p>
+                    <p>Condition: ${data.weather[0].description}</p>
+                    <p>Wind Speed: ${data.wind.speed} km/h</p>
+                `;
+            } else {
+                result.innerHTML = '<p>City not found</p>';
+            }
+        })
+        .catch(error => {
+            result.innerHTML = '<p>Something went wrong</p>';
+            console.log(error);
+        });
 }
